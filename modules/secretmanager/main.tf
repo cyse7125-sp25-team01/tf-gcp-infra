b89c7c7efc603bbb3b2b51e8a90709ac7866c4f0
Saimanas Gourabathini <gourabathini.s@northeastern.edu>
2025-03-28 22:30:07 +0000

    fix: secret added

--- modules/secretmanager/main.tf
+++ modules/secretmanager/main.tf
@@ -15,3 +15,21 @@ resource "google_secret_manager_secret_version" "password_version" {
 
   secret_data = var.postgres_password
 }
+
+resource "google_secret_manager_secret" "dockerhub-secret" {
+  secret_id = "dockerhub-secret"
+  replication {
+    user_managed {
+      replicas {
+        location = var.region
+      }
+    }
+
+  }
+}
+
+resource "google_secret_manager_secret_version" "dockerhub-secret_version" {
+  secret = google_secret_manager_secret.dockerhub-secret.id
+
+  secret_data = var.dockerhub-secret
+}
