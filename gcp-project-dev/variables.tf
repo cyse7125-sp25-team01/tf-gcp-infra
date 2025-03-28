a36fad83d4de3af0e192306e4147105eaf8813c1
Saimanas Gourabathini <gourabathini.s@northeastern.edu>
2025-03-28 22:36:48 +0000

    fix: secret added

--- gcp-project-dev/variables.tf
+++ gcp-project-dev/variables.tf
@@ -55,6 +55,11 @@ variable "postgres_password" {
   description = "Postgres password"
   type        = string
 }
+variable "dockerhub-secret" {
+  description = "Dockerhub secret"
+  type        = string
+}
+
 
 variable "secret_name" {
   description = "Secret Name"
